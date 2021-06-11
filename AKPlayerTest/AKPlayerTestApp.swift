35561f25a48c971409a53d5fb39fc1d152e58927
Hoopes <matthew.hoopes@gmail.com>
2021-06-10 20:47:12 -0400

    Initial Commit

---
+++ AKPlayerTest/AKPlayerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AKPlayerTestApp.swift
+//  AKPlayerTest
+//
+//  Created by Matthew Hoopes on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AKPlayerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
